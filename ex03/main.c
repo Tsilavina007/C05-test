0add344189d9cfd06d4b201766211ca6ac1f74f7
Tsilavina Gaettan Andriamiharison <tsilandr@c1r12s1.42antananarivo.mg>
2024-10-11 16:03:46 +0300

    update test

--- ex03/main.c
+++ ex03/main.c
@@ -8,7 +8,7 @@ int main(void) {
     printf("\n2**3 = 8 :           => your result = %d", ft_recursive_power(2, 3));
     printf("\n0**1 = 0 :           => your result = %d", ft_recursive_power(0, 1));
     printf("\n1**0 = 1 :           => your result = %d", ft_recursive_power(1, 0));
-    printf("\n4**0 = 1 :           => your result = %d", ft_recursive_power(4, 0));
+    printf("\n2**-1 = 0 :           => your result = %d", ft_recursive_power(2, -1));
     printf("\n10**10 = 1410065408 :=> your result = %d", ft_recursive_power(10, 10));
     printf("\n0**0 = 1 :           => your result = %d", ft_recursive_power(0, 0));
 
--- ex05/main.c
+++ ex05/main.c
@@ -5,7 +5,7 @@ int ft_sqrt(int index);
 int main(void) {
 
     printf("\nft_sqrt(0) = 0  :  => your result = %d", ft_sqrt(0));
-    printf("\nft_sqrt(1) = 0  :  => your result = %d", ft_sqrt(1));
+    printf("\nft_sqrt(1) = 1  :  => your result = %d", ft_sqrt(1));
     printf("\nft_sqrt(2) = 0  :  => your result = %d", ft_sqrt(2));
     printf("\nft_sqrt(3) = 0  :  => your result = %d", ft_sqrt(3));
     printf("\nft_sqrt(4) = 2  :  => your result = %d", ft_sqrt(4));
